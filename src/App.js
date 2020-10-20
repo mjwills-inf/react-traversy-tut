e1030d9c52da9ef9d0b841700bae13e3d52f748e
mjwills-inf <mjwills@live.co.uk>
2020-10-20 15:25:01 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Learn React',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Behave like a Grown Up',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Be peaceful',
+        completed: false
+      },
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
